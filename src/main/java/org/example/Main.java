6f5685008aec248f82c158af5e57e91c8ccea1d0
subhi-29 <subhiksha.b@tringapps.com>
2023-01-25 19:56:50 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,52 @@
+package org.example;
+import java.util.*;
+class Student{
+    public String stuname;
+    public String grade;
+    public double gpa;
+    public double gradeValue;
+    public double credit,mark;
+    Student(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\nEnter Your Name:");
+        stuname=sc.nextLine();
+        System.out.println("\nEnter the Grade level:");
+        grade=sc.nextLine();
+
+        System.out.println("\nEnter the Credit points:");
+        credit=sc.nextDouble();
+    }
+    String showdetails()
+    {
+        System.out.println("\nStudent Name:" +stuname);
+        System.out.println("\nGrade Level:" +grade);
+        return stuname+" has a "+gpa+" GPA";
+    }
+    void checkgrade(){
+
+
+        switch (grade) {
+            case "A" -> gradeValue = 4.00;
+            case "A-" -> gradeValue = 3.67;
+            case "B+" -> gradeValue = 3.33;
+            case "B" -> gradeValue = 3.00;
+            case "B-" -> gradeValue = 2.67;
+            case "C+" -> gradeValue = 2.33;
+            case "C" -> gradeValue = 2.00;
+            default -> System.out.println("Invalid Grade");
+        }
+    }
+    void updategpa(){
+        mark=gradeValue*credit;
+        gpa=mark/credit;
+    }
+
+    public static void main(String[] args){
+        Student s=new Student();
+
+        s.checkgrade();
+        s.updategpa();
+        System.out.println(s.showdetails());
+
+    }
+}
